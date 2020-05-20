dbb28354dec0a8b06be935e867f576aaa318fe8b
Saulo Medeiros de Araujo <saulo2@gmail.com>
2020-05-19 21:17:24 -0300

    https://github.com/mlflow/mlflow/issues/1815#issuecomment-566604566

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    with mlflow.start_run():
+    with mlflow.start_run(run_id=run_id, run_name=experiment_name):
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
